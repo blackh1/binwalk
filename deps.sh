632f7d7d4fc0b617be8df65f050d27cb076a2b2c
blackh1 <74771265+blackh1@users.noreply.github.com>
2025-08-20 00:26:46 +0800

    change deps.sh into uv manage

--- deps.sh
+++ deps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Check for the --yes command line argument to skip yes/no prompts
-if [ "$1" = "--yes" ]
-then
+if [ "$1" = "--yes" ]; then
     YES=1
 else
     YES=0
@@ -12,15 +11,12 @@ set -eu
 set -o nounset
 set -x
 
-if ! which lsb_release > /dev/null
-then
+if ! which lsb_release > /dev/null 2>&1; then
     function lsb_release {
-        if [ -f /etc/os-release ]
-        then
+        if [ -f /etc/os-release ]; then
             [[ "$1" = "-i" ]] && cat /etc/os-release | grep ^"ID" | cut -d= -f 2
             [[ "$1" = "-r" ]] && cat /etc/os-release | grep "VERSION_ID" | cut -d= -d'"' -f 2
-        elif [ -f /etc/lsb-release ]
-        then
+        elif [ -f /etc/lsb-release ]; then
             [[ "$1" = "-i" ]] && cat /etc/lsb-release | grep "DISTRIB_ID" | cut -d= -f 2
             [[ "$1" = "-r" ]] && cat /etc/lsb-release | grep "DISTRIB_RELEASE" | cut -d= -f 2
         else
@@ -29,44 +25,37 @@ then
     }
 fi
 
-if [ $YES -eq 0 ]
-then
+if [ $YES -eq 0 ]; then
     distro="${1:-$(lsb_release -i|cut -f 2)}"
     distro_version="${1:-$(lsb_release -r|cut -f 2|cut -c1-2)}"
 else
     distro="${2:-$(lsb_release -i|cut -f 2)}"
     distro_version="${2:-$(lsb_release -r|cut -f 2|cut -c1-2)}"
 fi
-REQUIRED_UTILS="wget tar python"
+REQUIRED_UTILS="wget tar"
 APTCMD="apt"
 APTGETCMD="apt-get"
 YUMCMD="yum"
-if [ $distro = "Kali" ]
-then
-    APT_CANDIDATES="git locales build-essential qt5base-dev mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract util-linux firmware-mod-kit cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop cpio"
-elif [ $distro_version = "14" ]
-then
-    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop srecord cpio"
-elif [ $distro_version = "15" ]
-then
-    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop srecord cpio"
-elif [ $distro_version = "16" ]
-then
-    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop srecord cpio"
-elif [ $distro_version = "18" ]
-then
-    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop srecord cpio"
+if [ "$distro" = "Kali" ]; then
+    APT_CANDIDATES="git locales build-essential qt5base-dev mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract util-linux firmware-mod-kit cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
+elif [ "$distro_version" = "14" ]; then
+    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
+elif [ "$distro_version" = "15" ]; then
+    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
+elif [ "$distro_version" = "16" ]; then
+    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsprogs cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
+elif [ "$distro_version" = "18" ]; then
+    APT_CANDIDATES="git locales build-essential libqt4-opengl mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
 else
-    APT_CANDIDATES="git locales build-essential qtbase5-dev mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk lzop srecord cpio"
+    APT_CANDIDATES="git locales build-essential qtbase5-dev mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract cramfsswap squashfs-tools zlib1g-dev liblzma-dev liblzo2-dev sleuthkit default-jdk"
 fi
 PYTHON3_APT_CANDIDATES=""
 PYTHON3_YUM_CANDIDATES=""
-YUM_CANDIDATES="git gcc gcc-c++ make openssl-devel qtwebkit-devel qt-devel gzip bzip2 tar arj p7zip p7zip-plugins cabextract squashfs-tools zlib zlib-devel lzo lzo-devel xz xz-compat-libs xz-libs xz-devel xz-lzma-compat python-backports-lzma lzip pyliblzma perl-Compress-Raw-Lzma lzop srecord"
-PYTHON="$(which python3)"
+YUM_CANDIDATES="git gcc gcc-c++ make openssl-devel qtwebkit-devel qt-devel gzip bzip2 tar arj p7zip p7zip-plugins cabextract squashfs-tools zlib zlib-devel lzo lzo-devel xz xz-compat-libs xz-libs xz-devel sleuthkit java-1.8.0-openjdk-devel"
+PYTHON="python3"
 
 # Check for root privileges
-if [ $UID -eq 0 ]
-then
+if [ $UID -eq 0 ]; then
     echo "UID is 0, sudo not required"
     SUDO=""
 else
@@ -74,11 +63,17 @@ else
     REQUIRED_UTILS="sudo $REQUIRED_UTILS"
 fi
 
+# 检查 uv 是否已安装
+if ! command -v uv &>/dev/null; then
+    echo "uv 未安装，请先在当前目录下安装 uv，然后再运行本脚本。"
+    exit 1
+fi
+
 function install_yaffshiv
 {
     git clone --quiet --depth 1 --branch "master" https://github.com/devttys0/yaffshiv
-    (cd yaffshiv && $SUDO $PYTHON setup.py install)
-    $SUDO rm -rf yaffshiv
+    (cd yaffshiv && uv pip install .)
+    rm -rf yaffshiv
 }
 
 function install_sasquatch
@@ -91,8 +86,8 @@ function install_sasquatch
 function install_jefferson
 {
     git clone --quiet --depth 1 --branch "master" https://github.com/sviehb/jefferson
-    (cd jefferson && $SUDO $PYTHON -mpip install -r requirements.txt && $SUDO $PYTHON setup.py install)
-    $SUDO rm -rf jefferson
+    (cd jefferson && uv pip install -r requirements.txt && uv pip install .)
+    rm -rf jefferson
 }
 
 function install_cramfstools
@@ -101,7 +96,6 @@ function install_cramfstools
   TIME=`date +%s`
   INSTALL_LOCATION=/usr/local/bin
 
-  # https://github.com/torvalds/linux/blob/master/fs/cramfs/README#L106
   git clone --quiet --depth 1 --branch "master" https://github.com/npitre/cramfs-tools
   # There is no "make install"
   (cd cramfs-tools \
@@ -112,20 +106,13 @@ function install_cramfstools
   rm -rf cramfs-tools
 }
 
-function install_pip_package
-{
-    PACKAGE="$1"
-    $SUDO $PYTHON -mpip install $PACKAGE
-}
-
 function find_path
 {
     FILE_NAME="$1"
 
     echo -ne "checking for $FILE_NAME..."
-    which $FILE_NAME > /dev/null
-    if [ $? -eq 0 ]
-    then
+    which $FILE_NAME > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
         echo "yes"
         return 0
     else
@@ -134,18 +121,23 @@ function find_path
     fi
 }
 
+# 安装 Python 依赖
+function install_python_deps
+{
+    uv pip install setuptools matplotlib capstone pycryptodome gnupg tk ubi_reader
+}
+
 # Make sure the user really wants to do this
-if [ $YES -eq 0 ]
-then
+if [ $YES -eq 0 ]; then
     echo ""
     echo "WARNING: This script will download and install all required and optional dependencies for binwalk."
     echo "         This script has only been tested on, and is only intended for, Debian based systems."
     echo "         Some dependencies are downloaded via unsecure (HTTP) protocols."
     echo "         This script requires internet access."
     echo "         This script requires root privileges."
+    echo "         Python 相关依赖将通过 uv 管理。"
     echo ""
-    if [ $distro != Unknown ]
-    then
+    if [ "$distro" != Unknown ]; then
         echo "         $distro $distro_version detected"
     else
         echo "WARNING: Distro not detected, using package-manager defaults"
@@ -153,13 +145,11 @@ then
     echo ""
     echo -n "Continue [y/N]? "
     read YN
-    if [ "$(echo "$YN" | grep -i -e 'y' -e 'yes')" == "" ]
-    then
+    if [ "$(echo "$YN" | grep -i -e 'y' -e 'yes')" == "" ]; then
         echo "Quitting..."
         exit 1
     fi
-elif [ $distro != Unknown ]
-then
+elif [ "$distro" != Unknown ]; then
      echo "$distro $distro_version detected"
 else
     echo "WARNING: Distro not detected, using package-manager defaults"
@@ -167,25 +157,20 @@ fi
 
 # Check to make sure we have all the required utilities installed
 NEEDED_UTILS=""
-for UTIL in $REQUIRED_UTILS
-do
+for UTIL in $REQUIRED_UTILS; do
     find_path $UTIL
-    if [ $? -eq 1 ]
-    then
+    if [ $? -eq 1 ]; then
         NEEDED_UTILS="$NEEDED_UTILS $UTIL"
     fi
 done
 
 # Check for supported package managers and set the PKG_* envars appropriately
 find_path $APTCMD
-if [ $? -eq 1 ]
-then
+if [ $? -eq 1 ]; then
     find_path $APTGETCMD
-    if [ $? -eq 1 ]
-    then
+    if [ $? -eq 1 ]; then
         find_path $YUMCMD
-        if [ $? -eq 1 ]
-        then
+        if [ $? -eq 1 ]; then
             NEEDED_UTILS="$NEEDED_UTILS $APTCMD/$APTGETCMD/$YUMCMD"
         else
             PKGCMD="$YUMCMD"
@@ -200,8 +185,7 @@ then
         PKG_PYTHON3_CANDIDATES="$PYTHON3_APT_CANDIDATES"
     fi
 else
-    if "$APTCMD" install -s -y dpkg > /dev/null
-    then
+    if "$APTCMD" install -s -y dpkg > /dev/null 2>&1; then
         PKGCMD="$APTCMD"
         PKGCMD_OPTS="install -y"
         PKG_CANDIDATES="$APT_CANDIDATES"
@@ -214,8 +198,7 @@ else
     fi
 fi
 
-if [ "$NEEDED_UTILS" != "" ]
-then
+if [ "$NEEDED_UTILS" != "" ]; then
     echo "Please install the following required utilities: $NEEDED_UTILS"
     exit 1
 fi
@@ -223,17 +206,16 @@ fi
 # Do the install(s)
 cd /tmp
 $SUDO $PKGCMD $PKGCMD_OPTS $PKG_CANDIDATES
-if [ $? -ne 0 ]
-    then
+if [ $? -ne 0 ]; then
     echo "Package installation failed: $PKG_CANDIDATES"
     exit 1
 fi
-install_pip_package "setuptools matplotlib capstone pycryptodome gnupg tk ubi_reader"
+
+install_python_deps
 install_sasquatch
 install_yaffshiv
 install_jefferson
 
-if [ $distro_version = "18" ]
-then
-install_cramfstools
+if [ "$distro_version" = "18" ]; then
+    install_cramfstools
 fi
